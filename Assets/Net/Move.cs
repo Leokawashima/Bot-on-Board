26d11d02889860b0a338aadf40dd1fcba034d3e3
DESKTOP-R0JRRCE\Leo <hypixel.block@gmail.com>
2023-06-19 22:55:18 +0900

    接続を試作

---
+++ Assets/Net/Move.cs
@@ -0,0 +1,44 @@
+using Unity.Netcode;
+using UnityEngine;
+
+public class Move : NetworkBehaviour
+{
+    [SerializeField] float m_moveSpeed = 5;
+
+    private Rigidbody m_rigidBody;
+    private Vector2 m_moveInput = Vector2.zero;
+
+    void Start()
+    {
+        m_rigidBody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if(IsOwner)
+        {
+            SetMoveInputServerRpc(
+                    Input.GetAxisRaw("Horizontal"),
+                    Input.GetAxisRaw("Vertical"));
+        }
+
+        if(IsServer)
+        {
+            ServerUpdate();
+        }
+    }
+
+    [ServerRpc]
+    private void SetMoveInputServerRpc(float x, float y)
+    {
+        m_moveInput = new Vector2(x, y);
+    }
+
+    private void ServerUpdate()
+    {
+        var velocity = Vector3.zero;
+        velocity.x = m_moveSpeed * m_moveInput.normalized.x;
+        velocity.z = m_moveSpeed * m_moveInput.normalized.y;
+        m_rigidBody.velocity = velocity;
+    }
+}
