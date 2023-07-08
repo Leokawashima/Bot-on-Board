0bca0926db0d50cd45d72fa7b50b05c43cf87252
Leo@MyHouse <hypixel.block@gmail.com>
2023-07-08 17:59:26 +0900

    Roomの画面構成を作成

--- Assets/22CI0212/RoomConection.cs
+++ Assets/22CI0212/RoomConection.cs
@@ -1,18 +1,18 @@
-using System.Collections;
 using System.Collections.Generic;
 using System.Net.Sockets;
 using System.Net;
 using System.Text;
-using Unity.Netcode;
 using UnityEngine;
 using TMPro;
 using System.Threading.Tasks;
 
+/// <summary>
+/// Roomの接続処理を賄うクラス
+/// </summary>
 public class RoomConection : MonoBehaviour
 {
     #region valiables
     [SerializeField] TMP_InputField text;
-    [SerializeField] uint maxUser = 4;
     [SerializeField] ushort port = 7777;
     [Header("Build")]
     [SerializeField] bool roomBuildContinueFlag = false;
@@ -32,66 +32,6 @@ public class RoomConection : MonoBehaviour
     [SerializeField] uint logSize = 20;
     List<string> logStr = new List<string>();
     #endregion
-
-    #region NGOevent
-    public void Server()
-    {
-        NetworkManager.Singleton.ConnectionApprovalCallback = ApprovalCheck;
-        NetworkManager.Singleton.StartServer();
-        text.text = GetLocalIPAddress();
-    }
-    public void Host()
-    {
-        NetworkManager.Singleton.ConnectionApprovalCallback = ApprovalCheck;
-        NetworkManager.Singleton.StartHost();
-        text.text = GetLocalIPAddress();
-    }
-    public void Client()
-    {
-        var transport = NetworkManager.Singleton.NetworkConfig.NetworkTransport;
-        if(transport is Unity.Netcode.Transports.UTP.UnityTransport unityTransport)
-        {
-            // 接続先のIPアドレスとポートを指定
-            var ipAddress = text.text;
-            unityTransport.SetConnectionData(ipAddress, port);
-        }
-        NetworkManager.Singleton.StartClient();
-    }
-    #endregion
-
-    void ApprovalCheck(NetworkManager.ConnectionApprovalRequest request, NetworkManager.ConnectionApprovalResponse response)
-    {
-        // 追加の承認手順が必要な場合は、追加の手順が完了するまでこれを true に設定します
-        // true から false に遷移すると、接続承認応答が処理されます。
-        response.Pending = true;
-
-        //最大人数をチェック(この場合は4人まで)
-        if(NetworkManager.Singleton.ConnectedClients.Count >= maxUser)
-        {
-            response.Approved = false;//接続を許可しない
-            response.Pending = false;
-            return;
-        }
-
-        //ここからは接続成功クライアントに向けた処理
-        response.Approved = true;//接続を許可
-
-        //PlayerObjectを生成するかどうか
-        response.CreatePlayerObject = true;
-
-        //生成するPrefabハッシュ値。nullの場合NetworkManagerに登録したプレハブが使用される
-        response.PlayerPrefabHash = null;
-
-        //PlayerObjectをスポーンする位置(nullの場合Vector3.zero)
-        var position = new Vector3(0, 1, -8);
-        position.x = -2 + 2 * (NetworkManager.Singleton.ConnectedClients.Count % 3);
-        response.Position = position;
-
-        //PlayerObjectをスポーン時の回転 (nullの場合Quaternion.identity)
-        response.Rotation = Quaternion.identity;
-
-        response.Pending = false;
-    }
 
     #region RoomEvent
     public async void StartRoom()
---
+++ Assets/22CI0212/RoomList.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Roomの接続リストを管理するクラス
+/// </summary>
+public class RoomList : MonoBehaviour
+{
+    RectTransform rect;
+
+    [SerializeField] GameObject memberPrefab;
+    [SerializeField] uint people = 2;
+    [SerializeField] Vector2 startPos = new Vector2(-620, 200);
+    [SerializeField] Vector2 offsetPos = new Vector2(0, -110);
+
+    void Start()
+    {
+        rect = transform as RectTransform;
+        Vector3 start = startPos * transform.lossyScale;
+        Vector3 offset = offsetPos * transform.lossyScale;
+
+        for (int i = 0; i < people; ++i)
+        {
+            var pos = rect.position + start + offset * i;
+            Instantiate(memberPrefab, pos, Quaternion.identity, transform);
+        }
+    }
+}
