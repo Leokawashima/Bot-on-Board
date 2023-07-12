5aa18adb6d05f64b8cc02c0b4a372a3a100d3f74
G.S <22ci0219@jec.ac.jp>
2023-07-12 16:01:36 +0900

    先行後行処理ほぼ完成

--- Assets/22CI0219/Script/Order.cs
+++ Assets/22CI0219/Script/Order.cs
@@ -13,44 +13,62 @@ public class Order : MonoBehaviour
         Follower //後行
     }
 
+    [SerializeField] Canvas OrderCanvas;
     [SerializeField] Button PrecedenceButton; //先行ボタン
     [SerializeField] Button FollowerButton; //後行ボタン
     [SerializeField] Button DecideButton; //決定ボタン
     [SerializeField] Image SelectImage; //選択したボタンが分かりやすいように作成した画像
+    [SerializeField] Text InductionText; //プレイヤーがするべきことへ誘導するテキスト
     protected StateOrder order = StateOrder.Non; //先行、後行が決まるまで何も決まっていない状態にしておく
+
+    [SerializeField] Text testText;
 
 
     // Start is called before the first frame update
     void Start()
     {
+        Debug.Log(order);
+
         PrecedenceButton.onClick.AddListener(Precedence_);
         FollowerButton.onClick.AddListener(Follower_);
         DecideButton.onClick.AddListener(Decide);
+
         SelectImage.enabled = false;
+        testText.enabled = false;
+
+        InductionText.text = "先行 後行を選んでください";
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+    //PrecedenceButtonが押されたとき
     void Precedence_()
     {
         SelectImage.enabled = true;
         SelectImage.transform.position = PrecedenceButton.transform.position + new Vector3(200, 98, 0);
         order = StateOrder.Precedence;
+        Debug.Log(order);
+
+        InductionText.text = "先行でよろしいでしょうか";
     }
 
+    //FollowerButtonが押されたとき
     void Follower_()
     {
         SelectImage.enabled = true;
         SelectImage.transform.position = FollowerButton.transform.position + new Vector3(200, 98, 0);
         order = StateOrder.Follower;
+        Debug.Log(order);
+
+        InductionText.text = "後行でよろしいでしょうか";
     }
 
+    //DecideButtonが押されたとき
     void Decide()
     {
-        gameObject.SetActive(false);
+        //先行、後行が選択されていない場合、この先の処理は行わない
+        if (order == StateOrder.Non) return;
+
+        OrderCanvas.enabled = false;
+        testText.enabled = true;
+        testText.text = "あなたは" + order;
     }
 }
