b90500a49b9f31f57c034740c33d5aa7bf36fc8f
blue_yama <0408ja2@gmail.com>
2023-07-18 05:10:53 +0900

    向きからベクトルを作る処理など実装中

--- Assets/22CI0201/Player_AI_Script.cs
+++ Assets/22CI0201/Player_AI_Script.cs
@@ -1,15 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
 public class Player_AI_Script : MonoBehaviour
 {
     [Header("一歩のサイズ")]
     [SerializeField] float one_steps_size;
+
     [SerializeField] private ParticleSystem particle;
 
+    [Header("現在の向き")]
+    [SerializeField] public Vector3 current_direction;
+
     //AIの状態
-    enum AI_state { non = 0, dead = 1, stun = 2, poison = 3, }
+    enum AI_state { non = 0, normal = 1, dead = 2, poison = 3, }
 
     //AIの向いている方向 (前後)
     enum AI_direction_FB { non = 0, forward = 1, back = -1, }
@@ -22,15 +27,21 @@ public class Player_AI_Script : MonoBehaviour
     AI_direction_LR direction_LR;
     AI_direction_FB direction_FB;
 
+    //現在のステートの状態からベクトルを作成する
+    public Vector3 direction
+    {
+        get { return new Vector3((int)direction_FB, 0,(int)direction_LR); }
+    }
+
     void Start()
     {
         one_steps_size_set(1f);
-
+        
         state = AI_state.non;
-        direction_FB = AI_direction_FB.forward;
-        direction_LR = AI_direction_LR.non;
+        direction_FB = AI_direction_FB.non;
+        direction_LR = AI_direction_LR.left;
 
-        state_check();
+        current_direction = direction;   
     }
 
     void Update()
@@ -38,20 +49,20 @@ public class Player_AI_Script : MonoBehaviour
         hoge++;
         if (hoge == 1000)
         {
-            go_forward_1steps();
+            go_1steps();
         }
     }
 
-//---------------------------------------------------------------------------------------------------------------------------------------------
-//以下、メソッド一覧
-//---------------------------------------------------------------------------------------------------------------------------------------------
-
+    //---------------------------------------------------------------------------------------------------------------------------------------------
+    //以下、メソッド一覧
+    //---------------------------------------------------------------------------------------------------------------------------------------------
+    #region 
     //１歩のサイズを初期化する
     void one_steps_size_set(float set_one_steps_size)
     {
         one_steps_size = set_one_steps_size;
     }
-
+    
     //パーティクルエフェクトを再生する
     void play_particle_effect()
     {
@@ -64,33 +75,42 @@ public class Player_AI_Script : MonoBehaviour
         //未実装
     }
 
+    //現在の向きに1歩進む
+    void go_1steps()
+    {
+        current_direction = direction;
+        play_particle_effect();
+        transform.position += transform.TransformDirection(current_direction) * one_steps_size;
+        UnityEngine.Debug.Log("現在の向きへの移動が実行されました。");
+    }
+
     //１歩前に進む
     void go_forward_1steps()
     {
         play_particle_effect();
-        transform.position += transform.TransformDirection(Vector3.forward) * one_steps_size;
-        Debug.Log("実行されました。");
+        transform.position += transform.TransformDirection(current_direction) * one_steps_size;
+        UnityEngine.Debug.Log("実行されました。");
     }
 
     //１歩後ろに下がる
     void go_back_1steps()
     {
         transform.position += transform.TransformDirection(Vector3.back) * one_steps_size;
-        Debug.Log("実行されました。");
+        UnityEngine.Debug.Log("実行されました。");
     }
 
     //１歩右に進む
     void go_right_1steps()
     {
         transform.position += transform.TransformDirection(Vector3.right) * one_steps_size;
-        Debug.Log("実行されました。");
+        UnityEngine.Debug.Log("実行されました。");
     }
 
     //１歩左に進む
     void go_left_1steps()
     {
         transform.position += transform.TransformDirection(Vector3.left) * one_steps_size;
-        Debug.Log("実行されました。");
+        UnityEngine.Debug.Log("実行されました。");
     }
 
     //AIの向いている、左右の方向をチェックする
@@ -99,15 +119,15 @@ public class Player_AI_Script : MonoBehaviour
         switch(direction_LR)
         {
             case AI_direction_LR.non:
-                Debug.Log("AIは右も左も向いていません。");
+                UnityEngine.Debug.Log("AIは右も左も向いていません。");
                 break;
 
             case AI_direction_LR.left:
-                Debug.Log("AIは左を向いています。");
+                UnityEngine.Debug.Log("AIは左を向いています。");
                 break;
 
             case AI_direction_LR.right:
-                Debug.Log("AIは右を向いています。");
+                UnityEngine.Debug.Log("AIは右を向いています。");
                 break;
         }
     }
@@ -118,15 +138,15 @@ public class Player_AI_Script : MonoBehaviour
         switch (direction_FB)
         {
             case AI_direction_FB.non:
-                Debug.Log("AIは前も後ろも向いていません。");
+                UnityEngine.Debug.Log("AIは前も後ろも向いていません。");
                 break;
 
             case AI_direction_FB.forward:
-                Debug.Log("AIは前を向いています。");
+                UnityEngine.Debug.Log("AIは前を向いています。");
                 break;
 
             case AI_direction_FB.back:
-                Debug.Log("AIは後ろを向いています。");
+                UnityEngine.Debug.Log("AIは後ろを向いています。");
                 break;
         }
     }
@@ -137,20 +157,21 @@ public class Player_AI_Script : MonoBehaviour
         switch(state)
         {
             case AI_state.non:
-                Debug.Log("AIに状態異常はありません。");
+                UnityEngine.Debug.Log("AIの状態が存在しません。");
+                break;
+
+            case AI_state.normal:
+                UnityEngine.Debug.Log("AIに状態異常はありません。");
                 break;
 
             case AI_state.dead:
-                Debug.Log("AIは死にました。");
+                UnityEngine.Debug.Log("AIは死亡しています。");
                 break;
 
             case AI_state.poison:
-                Debug.Log("AIは毒状態になっています。");
-                break;
-
-            case AI_state.stun:
-                Debug.Log("AIは気絶しています。");
+                UnityEngine.Debug.Log("AIは毒状態になっています。");
                 break;
         }
     }
+    #endregion
 }
