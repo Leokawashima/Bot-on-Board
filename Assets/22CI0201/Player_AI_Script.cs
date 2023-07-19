c9093daa19916b59b31e34b4c87870455841ebd4
22CI0201 <22CI0201@jec.ac.jp>
2023-07-19 16:45:44 +0900

    ワークフローの内容を実装。マップチェック関数、アニメーション等を進行中。

--- Assets/22CI0201/Player_AI_Script.cs
+++ Assets/22CI0201/Player_AI_Script.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using Palmmedia.ReportGenerator.Core.Reporting.Builders;
+using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using UnityEngine;
@@ -6,42 +7,45 @@ using UnityEngine;
 public class Player_AI_Script : MonoBehaviour
 {
     [Header("一歩のサイズ")]
-    [SerializeField] float one_steps_size;
+    [SerializeField] private float one_steps_size;
 
     [SerializeField] private ParticleSystem particle;
 
     [Header("現在の向き")]
-    [SerializeField] public Vector3 current_direction;
+    [SerializeField] private Vector3 current_direction;
+
+    [Header("マップの有無を確認する変数")]
+    [SerializeField] private bool exists_map;
 
     //AIの状態
-    enum AI_state { non = 0, normal = 1, dead = 2, poison = 3, }
+    public enum AI_state { non = 0, normal = 1, dead = 2, poison = 3, }
 
     //AIの向いている方向 (前後)
-    enum AI_direction_FB { non = 0, forward = 1, back = -1, }
+    public enum AI_direction_FB { non = 0, forward = 1, back = -1, }
 
     //AIの向いている方向 (左右)
-    enum AI_direction_LR { non = 0, left = 1, right = -1, }
+    public enum AI_direction_LR { non = 0, left = 1, right = -1, }
 
     int hoge;
     AI_state state;
     AI_direction_LR direction_LR;
     AI_direction_FB direction_FB;
+    Vector3 AIpos;
 
     //現在のステートの状態からベクトルを作成する
-    public Vector3 direction
+    private Vector3 direction
     {
-        get { return new Vector3((int)direction_FB, 0,(int)direction_LR); }
+        get { return new Vector3((int)direction_LR, 0, (int)direction_FB); }
     }
 
     void Start()
     {
-        one_steps_size_set(1f);
-        
-        state = AI_state.non;
-        direction_FB = AI_direction_FB.non;
-        direction_LR = AI_direction_LR.left;
+        One_steps_size = 1f;
 
-        current_direction = direction;   
+        Initialized(1f, 1, 0, 1);
+
+        current_direction = direction;
+
     }
 
     void Update()
@@ -50,19 +54,31 @@ public class Player_AI_Script : MonoBehaviour
         if (hoge == 1000)
         {
             go_1steps();
+            state_check();
         }
     }
 
     //---------------------------------------------------------------------------------------------------------------------------------------------
-    //以下、メソッド一覧
+    //以下、メソッドなど
     //---------------------------------------------------------------------------------------------------------------------------------------------
+
+    //メソッド
     #region 
     //１歩のサイズを初期化する
-    void one_steps_size_set(float set_one_steps_size)
+    private void one_steps_size_set(float set_one_steps_size)
     {
         one_steps_size = set_one_steps_size;
     }
-    
+
+    //生成時に値を設定する
+    public void Initialized(float one_steps_size, int directionFB, int directionLR, int AIstate)
+    {
+        One_steps_size = one_steps_size;
+        Direction_FB = (AI_direction_FB)directionFB;
+        Direction_LR = (AI_direction_LR)directionLR;
+        State = (AI_state)AIstate;
+    }
+
     //パーティクルエフェクトを再生する
     void play_particle_effect()
     {
@@ -172,6 +188,61 @@ public class Player_AI_Script : MonoBehaviour
                 UnityEngine.Debug.Log("AIは毒状態になっています。");
                 break;
         }
+    }
+
+    //マップの有無を確認する
+    bool map_check()
+    {
+        if (exists_map)
+        {
+            return true;
+        }
+        return false;
+    }
+    #endregion
+
+    //各変数のget/set
+    #region
+    //1歩のサイズのget/set
+    public float One_steps_size
+    {
+        get { return one_steps_size; }
+        protected set { one_steps_size = value; }
+    }
+
+    //現在の向きのget/set
+    public Vector3 Current_direction
+    {
+        get { return current_direction; }
+        protected set { current_direction = value; }
+    }
+
+    //マップの有無のget/set
+    public bool Exists_map
+    {
+        get { return exists_map; }
+        protected set { exists_map = value; }
+    }
+
+    //AIの状態のget/set
+    public AI_state State
+    {
+        get { return state; }
+        protected set { state = value; }
+    }
+
+    //AIの前後の向きのget/set
+    public AI_direction_FB Direction_FB
+    {
+        get { return direction_FB; }
+        protected set { direction_FB = value; }
+    }
+
+    //AIの左右の向きのget/set
+    public AI_direction_LR Direction_LR
+    {
+        get { return direction_LR; }
+        protected set { direction_LR = value; }
     }
     #endregion
 }
