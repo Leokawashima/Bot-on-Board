b6f79717426647a6d05138265a76ce7fd645af8c
G.S <22ci0219@jec.ac.jp>
2023-07-19 14:54:49 +0900

    親クラスから継承させるところまで作成

---
+++ Assets/22CI0219/Script/Item.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// アイテムの親クラス
+/// </summary>
+public abstract class Item : MonoBehaviour
+{
+    public abstract void Action();
+}
