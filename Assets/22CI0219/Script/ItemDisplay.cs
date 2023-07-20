be570930dd2a224774924df295b1ee614c45fd29
22CI0219 <mirusui0804@gmail.com>
2023-07-20 10:20:14 +0900

    アイテムを表示するボタン作成

---
+++ Assets/22CI0219/Script/ItemDisplay.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// アイテムをボタン内に表示するスクリプト
+/// </summary>
+public class ItemDisplay : MonoBehaviour
+{
+    Text ButtonText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ButtonText = gameObject.GetComponentInChildren<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ButtonText.text = "回復";
+    }
+}
