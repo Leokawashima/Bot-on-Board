22f483a4b8bdf50a8f9ea65eeb3d9bec857cac7c
ak <CI@jec.ac.jp>
2023-09-26 09:54:39 +0900

    Tweet機能実装可能　学校のPCで展示することなどを考えて生成したTweetリンクをQRか何かに出力してスマホで読み取ってツイートさせたいがQRは少し難しそうなのでいったん放置

---
+++ Assets/Script/Test/TweetScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class TweetScript : MonoBehaviour
+{
+    
+    [SerializeField] string text = "TweetTest";
+
+    const string
+        newText = "text=",
+        newUrl = "&url=",
+        newHashTag = "&hashtags=",
+        newLine = "%0a",
+        space = "%20",
+        sharp = "%23";
+
+    [ContextMenu("Tweet")]
+    void Tweet()
+    {
+        var url = "https://twitter.com/intent/tweet?"
+            + "text=" + text
+            + newLine + newHashTag + "BotonBoard"
+            + newLine + newHashTag + "BoB";
+
+        Debug.Log(url);
+
+#if UNITY_WEBGL
+        Application.ExternalEval(string.Format("window.open('{0}','_blank')", url));
+#else
+        Application.OpenURL(url);
+#endif
+    }
+}
