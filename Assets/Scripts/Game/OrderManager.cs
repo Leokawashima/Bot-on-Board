21629ee45832de578a0cfee58c032a1005f5cc5c
ak <CI@jec.ac.jp>
2023-10-04 16:09:34 +0900

    大まかな構成が完成　調整の一歩手前

---
+++ Assets/Scripts/Game/OrderManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class OrderManager : MonoBehaviour
+{
+    enum OrderState { Normal, GoAttack, SaveLife, }
+
+    private void Start()
+    {
+        //ActiveToggles().FirstOrDefault();
+    }
+}
