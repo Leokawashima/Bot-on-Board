e788edca76d1cdc19ebb11cea7313053483a064b
22ci0209 <22CI0209@jec.ac.jp>
2024-01-22 15:53:47 +0900

    テストレベル進捗 ゲーミングスクリプト追加

---
+++ Assets/Scenes/TestLevel/0209/Gamer/Gamer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gamer : MonoBehaviour
+{
+    [Header("ここに現在のマテリアルをドロップ")]
+    [SerializeField]Material material;
+    [Header("虹色に光るスピード（0.001を推奨）")]
+    [SerializeField]float gamingSpeed = 0.001f;
+    float hue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hue = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hue += gamingSpeed;
+        if (hue >= 1.0f) hue = 0;
+        material.color = Color.HSVToRGB(hue, 1, 1);
+    }
+}
